d99dc107b2cc800be06bc48801fc3f1a40ba5bb4
Daniele Margiacchi <danielemargiacchi@gmail.com>
2025-02-07 17:20:27 +0100

    fix shipment date

--- src/Components/ShipmentsList/ShipmentsList.tsx
+++ src/Components/ShipmentsList/ShipmentsList.tsx
@@ -3,13 +3,18 @@ import useShipmentsApi from "../../hooks/useShipmentsApi";
 const ShipmentsList = () => {
   const { shipments } = useShipmentsApi();
 
+  const formatDate = (dateString: string) => {
+    const date = new Date(dateString);
+    return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+  };
+
   return (
     <>
       {shipments.map((shipment) => (
         <div key={shipment.id_shipping}>
           <h3>Corriere: {shipment.courier}</h3>
-          <p>Data di creazione: {shipment.creation_date}</p>
-          <p>Stato: {shipment.status_name}</p>
+          <p>Data di creazione: {formatDate(shipment.creation_date)}</p>
+          <p>Stato: {}</p>
           <p>ID Cliente: {shipment.customer_id}</p>
         </div>
       ))}
